e05379a27739887c664ad1fd0b5c8bb3350d21a6
SirOttar <101106388+SirOttar@users.noreply.github.com>
2022-03-18 11:05:54 +0100

    rechenoperation

--- taschenrechner/script.js
+++ taschenrechner/script.js
@@ -1,4 +1,65 @@
+var aktuelleZahl = "";
+var opera = "";
+var zahl1 = "";
+
 function drueckeZahl(zahl) {
-  var ergebnis = document.getElementById("ergebnis");
-  ergebnis.innerHTML = zahl;
+  var stringZahl = zahl.toString();
+  aktuelleZahl += stringZahl;
+  var ergebnisFeld = document.getElementById("ergebnis");
+  ergebnisFeld.innerHTML = aktuelleZahl;
+}
+
+function addition(number1, number2) {
+  return number1 + number2;
+}
+
+function subtraktion(number1, number2) {
+  return number1 - number2;
+}
+
+function multiplikation(number1, number2) {
+  return number1 * number2;
+}
+
+function division(number1, number2) {
+  return number1 / number2;
+}
+
+function setOperation(operation) {
+  opera = operation;
+  zahl1 = parseInt(aktuelleZahl);
+  aktuelleZahl = "";
+}
+
+function gleichheitszeichen() {
+  var ergebnis = 0;
+  var zahl2 = parseInt(aktuelleZahl);
+  switch (opera) {
+    case "+":
+      ergebnis = addition(zahl1, zahl2);
+      break;
+    case "-":
+      ergebnis = subtraktion(zahl1, zahl2);
+      break;
+    case "*":
+      ergebnis = multiplikation(zahl1, zahl2);
+      break;
+    case "/":
+      ergebnis = division(zahl1, zahl2);
+      break;
+    default:
+      return;
+  }
+  aktuelleZahl = ergebnis;
+  var ergebnisFeld = document.getElementById("ergebnis");
+  ergebnisFeld.innerHTML = aktuelleZahl;
+  opera = "";
+}
+
+function del() {
+  var ergebnisFeld = document.getElementById("ergebnis");
+  ergebnisFeld.innerHTML = "";
+  opera = "";
+  aktuelleZahl = "";
+  zahl1 = "";
 }
